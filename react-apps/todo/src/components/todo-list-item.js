d7f59ef2bfd66c5f01e49667f2a993806acc9a24
Alexander Pavlovets <olpav@ciklum.com>
2019-05-08 23:44:42 +0300

    props object

--- react-apps/todo/src/components/todo-list-item.js
+++ react-apps/todo/src/components/todo-list-item.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
-const TodoListItem = () => <span>Drink coffee</span>
+/**
+ * 
+ * @param props // object. Got from <li><TodoListItem label="Drink coffee"/></li>
+ * here it is just restructured 
+ *  
+ */
+const TodoListItem = ({label, important = false}) => {
+  
+  const style = {
+    color: important ? 'tomato' : 'black'
+  }
 
+  return <span style={style}>{label}</span>
+}
 export default TodoListItem
