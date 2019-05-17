960dff11f521d0abac9fbae03f95874c3bd0ff7e
Alexander Pavlovets <olpav@ciklum.com>
2019-05-17 19:03:14 +0300

    forms basics also here example of setState Wo cb

--- react-apps/todo/src/components/item-add-form/item-add-form.js
+++ react-apps/todo/src/components/item-add-form/item-add-form.js
@@ -3,18 +3,35 @@ import './item-add-form.css'
 
 export default class ItemAddForm extends Component {
 
+  state = {
+    label: ''
+  }
+
+  onLabelChange = (event) => {
+    this.setState({
+      label: event.target.value
+    })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.props.onItemAdded(this.state.label)
+  }
+
   render() {
-
-    const {onItemAdded} = this.props
-
     return (
-      <div className="item-add-form">
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done">
+        </input>
         <button
-          className="btn btn-outline-secondary"
-          onClick={() => onItemAdded('item data to add')}>
+          className="btn btn-outline-secondary">
           Add Item
         </button>
-      </div> 
+      </form> 
     )
   }
 }
