5abeee6a9e1043b9bdc4460e0cfd6ed252427bcc
Alexander Pavlovets <olpav@ciklum.com>
2019-05-28 18:59:11 +0300

    Error handling i do not like this way

--- react-apps/star-db/src/index.js
+++ react-apps/star-db/src/index.js
@@ -1,1 +1,17 @@
 console.log('Hello from React')
+
+const getResource = async (url) => {
+  const resp = await fetch(url)
+
+  if (!resp.ok) {
+    throw new Error(`Could not fetch ${url}. Got ${resp.status}`)
+  }
+  const body = await resp.json()
+  return body
+}
+
+getResource('https://swapi.co/api/people/1asdasd')
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err))
+
+console.log('React app')
