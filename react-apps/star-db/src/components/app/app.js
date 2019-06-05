16c8273d500a21de13126af535a71a20861c709a
Alexander Pavlovets <olpav@ciklum.com>
2019-06-05 09:05:42 +0300

    Love react but have to stop. Work related technologies in priority

--- react-apps/star-db/src/components/app/app.js
+++ react-apps/star-db/src/components/app/app.js
@@ -32,8 +32,6 @@ export default class App extends Component {
         <RandomPlanet />
         <ErrorButton />
         <PeoplePage />
-        <PeoplePage />
-        <PeoplePage />
       </div>
     )
   }
